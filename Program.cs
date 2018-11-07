d001a4b9dd5f2dcb6034af1315bcdec5be89cf4a
Diogo Gomes <dgomes@graphnode.com>
2018-11-07 01:24:04 +0000

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CI!");
+        }
+    }
+}
